b94876ecd81231e3326cf9804a9291347d132a4d
Shameem1212 <shameempourostadkar@gmail.com>
2025-04-10 13:34:45 -0700

    Commit

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,25 +1,30 @@
-describe("Quiz Application", () => {
-  beforeEach(() => {
-    cy.visit("http://127.0.0.1:3002/");
-  });
+import React from "react";
+import { mount } from "cypress/react";
+import Quiz from "../../client/src/components/Quiz";
 
+describe("Quiz Component", () => {
   it("Displays the Start Quiz button", () => {
+    mount(<Quiz />);
     cy.contains("Start Quiz").should("be.visible");
   });
   it("Start Quiz", () => {
+    mount(<Quiz />);
     cy.contains("Start Quiz").click();
   });
   it("Displays Question & Answers", () => {
+    mount(<Quiz />);
     cy.contains("Start Quiz").click();
     cy.get(".card").should("be.visible");
     cy.get(".btn-primary").should("have.length", 4);
   });
   it("Displays next question after answering", () => {
+    mount(<Quiz />);
     cy.contains("Start Quiz").click();
     cy.get(".btn-primary").first().click();
     cy.get(".card").should("be.visible");
   });
   it("Quiz Completed", () => {
+    mount(<Quiz />);
     cy.contains("Start Quiz").click();
     cy.get(".btn-primary").first().click();
     cy.get(".btn-primary").first().click();
@@ -34,6 +39,7 @@ describe("Quiz Application", () => {
     cy.contains("Quiz Completed").should("be.visible");
   });
   it("Quiz Completed and Score is Shown", () => {
+    mount(<Quiz />);
     cy.contains("Start Quiz").click();
     cy.get(".btn-primary").first().click();
     cy.get(".btn-primary").first().click();
@@ -49,6 +55,7 @@ describe("Quiz Application", () => {
     cy.contains("Your score:").should("be.visible");
   });
   it("Quiz Completed and Can Take New Quiz", () => {
+    mount(<Quiz />);
     cy.contains("Start Quiz").click();
     cy.get(".btn-primary").first().click();
     cy.get(".btn-primary").first().click();
--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,19 +1,23 @@
-import db from '../config/connection.js';
-import { Question } from '../models/index.js'
-import cleanDB from './cleanDb.js';
+import db from "../config/connection.js";
+import { Question } from "../models/index.js";
+import cleanDB from "./cleanDb.js";
+import fs from "fs/promises";
+import path from "path";
 
-import questionData from './pythonQuestions.json' assert{ type: 'json'};
+const questionDataPath = path.resolve("./src/seeds/pythonQuestions.json");
 
 try {
+  const questionData = JSON.parse(await fs.readFile(questionDataPath, "utf-8"));
+
   await db();
   await cleanDB();
 
   // bulk create each model
   await Question.insertMany(questionData);
 
-  console.log('Seeding completed successfully!');
+  console.log("Seeding completed successfully!");
   process.exit(0);
 } catch (error) {
-  console.error('Error seeding database:', error);
+  console.error("Error seeding database:", error);
   process.exit(1);
 }
